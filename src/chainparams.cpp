d4eea235f08ca543baf8d0adfef775616cf8b99e
cryptforall <36750226+cryptforall@users.noreply.github.com>
2018-12-11 19:03:34 -0500

    Fork at 82k

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -99,7 +99,7 @@ public:
         consensus.BIP34Hash = uint256S("0x000007d91d1254d60e2dd1ae580383070a4ddffa4c64c2eeb4a2f9ecc0414343");
         consensus.powLimit = uint256S("0007ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.nZawyLwmaAveragingWindow = 60;
-        consensus.XbufferTSA =   //decide your block, new coins place it after N
+        consensus.XbufferTSA = 82000;   //decide your block, new coins place it after N
         consensus.nPowTargetTimespan = 24 * 60 * 60 * 8; // Xchange: 1 day
         consensus.nPowTargetSpacing = 150;
         consensus.fPowAllowMinDifficultyBlocks = false;
