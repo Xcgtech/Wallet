2324aef877282254f62f4ff68f6ef3a4fafc16e1
cryptforall <36750226+cryptforall@users.noreply.github.com>
2018-12-13 12:53:07 -0500

    Revert "protocol update"
    This reverts commit 8d64ce8d3f7438095dd7675c68abe713456ca5d9.

--- src/version.h
+++ src/version.h
@@ -10,7 +10,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70786;
+static const int PROTOCOL_VERSION = 70301;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -19,15 +19,15 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70786;
+static const int MIN_PEER_PROTO_VERSION = 70301;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
 static const int CADDR_TIME_VERSION = 31402;
 
 //! only request blocks from nodes outside this range of versions
-static const int NOBLKS_VERSION_START = 80600;
-static const int NOBLKS_VERSION_END = 83500;
+static const int NOBLKS_VERSION_START = 32000;
+static const int NOBLKS_VERSION_END = 32400;
 
 //! BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
