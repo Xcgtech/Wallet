07183b5f4d7f48dc21a2f15284cca910e27999fa
Znuff <znuffy@gmail.com>
2019-01-03 22:40:42 +0000

    Don't touch.
    This reverts commit 270888c1972c2532622c61e0e39a7dca21fb50a5.

--- contrib/mn-linux-upgrade20181217.sh
+++ contrib/mn-linux-upgrade20181217.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-RELEASEFILE="https://github.com/Xcgtech/Wallet/releases/download/0.24.1/Xchangecore-0.24.1.tar.gz"
+RELEASEFILE="https://github.com/Xcgtech/Wallet/releases/download/0.24.0/Xchangecore-0.24.0.tar.gz"
 SENTINELGIT="https://github.com/cryptforall/sentinel.git" # leave empty if coin has no sentinel
 
 instance="" # only when doing multiple instances
 daemon="Xchanged"
 cli="Xchange-cli"
 stopcli="stop"
-archive_path="Xchangecore-0.24.1/bin/"
+archive_path="Xchangecore-0.24.0/bin/"
 core_dir=".Xchangecore"
 config_path="$core_dir/Xchange.conf"
 node_user="xcg""$instance"
@@ -119,7 +119,7 @@ start_services
 # set checkpoint
 echo "$checkpoint" > "$installer_checkpoint"
 echo -e "\n"
-echo "We have updated to version$tyellow 0.24.1$treset."
+echo "We have updated to version$tyellow 0.24.0$treset."
 echo "$tred""IF$treset you ever need to$tmagenta reindex$treset the blockchain - please use:$tyellow systemctl start $daemon-reindex$treset"
 echo -e "\n"
 echo "Done. Updated to checkpoint: $tyellow$checkpoint$treset".
